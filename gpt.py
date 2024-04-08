b586efb6273aff3f238053f9e9c93ebc252fac58
Alexandre Castro <alexandre.acrsf@gmail.com>
2024-04-08 13:22:41 -0300

    handling errors

--- gpt.py
+++ gpt.py
@@ -1,14 +1,16 @@
 from env import API_KEY
 import requests
 
-
 count = 0
+
 def get_similarity_score(definition1, definition2):
     global count
-    
+
     headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
     link = 'https://api.openai.com/v1/chat/completions'
     model_id = 'gpt-3.5-turbo'
+
+    score_str = ''
 
     prompt = f"Classifique o nível de similaridade entre estes textos de 0-100 e se eles definem a mesma coisa(responda com o padrão  de output: [score: 100, 'sim']):\n\nTexto 1: {definition1}\n\nTexto 2: {definition2}"
 
@@ -34,8 +36,10 @@ def get_similarity_score(definition1, definition2):
     except KeyError as e:
         print(f"Error processing response: {e}")
         score = 0
+    except ValueError as e:
+        print(f"Error converting score to int: {e}")
+        score = 0
 
-    print(f"score:{ score}, \n response: {score_str}, \n count: {count}")
+    print(f"score: {score}, \nresponse: {score_str}, \ncount: {count}")
 
     return score
-
